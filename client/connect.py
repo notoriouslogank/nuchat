38df29937a8e8e339baa70f8f7b5f1d9ced6f39d
logank <logan.aker1989@gmail.com>
2023-03-29 04:22:55 -0400

    Uh honestly I don't remember

--- client/connect.py
+++ client/connect.py
@@ -1,100 +1,23 @@
 import socket
 import threading
 
-host = ("192.168.0.238", 65522)
-stop_thread = False
+HEADER = 2048
+FORMAT = 'ascii'
+SERVER = '192.168.0.238'
+PORT = 65522
+ADDR = (SERVER, PORT)
+DISCONNECT_MSG = '!quit'
 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
-def usr_acct():
-    """Take nickname, check it against database, print to client."""
-    global nickname
-    global password
-
-    print("Choose a nickname.")
-    print("If blank, a nickname will be randomly")
-    print("assigned to you. \n")
-    nickname = input("Nickname: ")
-
-    if nickname == "ADMIN":
-        password = input("Enter password for user ADMIN: ")
-
-
-def usr_online():
-    """Allow the client connection and keep it open."""
-    global client
-    global stop_thread
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(host)
-
-    stop_thread = False
-
-
-def response():
-    while True:
-        global stop_thread
-        global client
-        if stop_thread:
-            break
-        try:
-            message = client.recv(1024).decode("ascii")
-            if message == "KICK":
-                client.send(nickname.encode("ascii"))
-                next_message = client.recv(1024).decode("ascii")
-                if next_message == "PASS":
-                    client.send(password.encode("ascii"))
-                    if client.recv(1024).decode("ascii") == "REFUSE":
-                        print("Connection refused; incorrect login information.")
-                        stop_thread = True
-                elif next_message == "BAN":
-                    print("Connection refused: BANNED!")
-                    client.close()
-                    stop_thread = True
-            else:
-                print(f"{nickname} : {message}")
-                print("")
-        except:
-            print("An error occurred.  Whoopsie-daisy!")
-            stop_thread = True
-            client.close()
-            break
-
-
-def command():
-    """ Commands
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
     
-    # TODO: This could probably be made into a class relatively easily, eh?  Possibly move it into its own 'commands' module.
-    """
-    global nickname
-    global stop_thread
-    while True:
-        if stop_thread:
-            break
-        message = f'{nickname}: {input(" ")} \n'
-        if message[len(nickname) + 2 :].startswith("!"):
-            if message[len(nickname) + 2 :].startswith('!quit'):
-                print("Goodbye!")
-                client.close()
-                stop_thread = True
-                break
-            elif nickname == "ADMIN":
-                if message[len(nickname) + 2 :].startswith("!kick"):
-                    client.send(f"KICK {message[len(nickname)+2+6:]}".encode("ascii"))
-                elif message[len(nickname) + 2 :].startswith("!ban"):
-                    client.send(f"BAN {message[len(nickname)+2+5:]}".encode("ascii"))
-            else:
-                print("This command can only be executed by ADMIN!")
-        else:
-            client.send(message.encode("ascii"))
-            print('\n')
-
-
-def main():
-    usr_acct()
-    usr_online()
-    response_thread = threading.Thread(target=response)
-    response_thread.start()
-    write_thread = threading.Thread(target=command)
-    write_thread.start()
-
-
-main()
+while True:
+    send(input())
--- server/start.py
+++ server/start.py
@@ -1,124 +1,56 @@
-import os
 import socket
 import threading
-from datetime import datetime
 
-host = ()  # Server IP
-port = ()  # Server Port
-banlist = "server/banlist.txt"  # Where to save the list of banned users <./server/banlist.txt>
+HEADER = 2048
+FORMAT = "ascii"
+DISCONNECT_MSG = "!quit"
+HOST = ()
+PORT = ()
+ADDR = ()
 
-clients = []
-nicknames = []
 
-
-def getHost():
+def hostInfo():
     """Automatically configure IP"""
-
-    global host
-    global port
+    global HOST
+    global PORT
+    global ADDR
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    host = s.getsockname()[0]
-    port = 65522  # TODO: Is this best practices?
-    hostname = host, port
+    HOST = s.getsockname()[0]
+    PORT = 65522  # TODO: Is this best practices?
+    ADDR = HOST, PORT
     s.close()
-    return hostname
+    return ADDR
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((getHost()))
-server.listen()
+server.bind(hostInfo())
 
 
-def timestamp():
-    """Return an ascii encoded, formatted string with current datetime for user as timestamp."""
-    time = datetime.now()
-    now = time.strftime("%H:%M:%S")
-    return str(now)
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MSG:
+                connected = False
+                conn.close()
+            print(f"[{addr}] {msg}")
 
 
-def broadcast(message):
-    """Send a message to all clients."""
-    for client in clients:
-        client.send(message)
-
-
-def respond(client):
-    """Decode the client message"""
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {HOST}.")
     while True:
-        try:
-            msg = message = client.recv(1024)
-            if msg.decode("ascii").startswith("KICK"):
-                if nicknames[clients.index(client)] == "ADMIN":
-                    name_to_kick = msg.decode("ascii")[5:]
-                    kick_user(name_to_kick)
-                else:
-                    client.send("Command refused; must be ADMIN!".encode("ascii"))
-            elif msg.decode("ascii").startswith("BAN"):
-                if nicknames[clients.index(client)] == "ADMIN":
-                    name_to_ban = msg.decode("ascii")[4:]
-                    kick_user(name_to_ban)
-                    with open(f"{banlist}", "a") as f:
-                        f.write(f"{name_to_ban}\n")
-                    print(f"{name_to_ban} was banned.")
-                else:
-                    client.send("Command refused; must be ADMIN!".encode("ascii"))
-            elif msg.decode("ascii").startswith("CLS"):
-                if nicknames[clients.index(client)] == "ADMIN":
-                    os.system("clear")
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f"{nickname} left the chat.".encode("ascii"))
-            nicknames.remove(nickname)
-            break
-
-
-def auth_connection():
-    while True:
-        client, address = server.accept()
-        print(f"Connection from: {str(address)}.")
-        nickname = client.recv(1024).decode("ascii")
-
-        with open(f"{banlist}", "r") as f:
-            bans = f.readlines()
-
-        if (f'{nickname} + /n') in bans:
-            client.send("BAN".encode("ascii"))
-            client.close()
-            continue
-
-        if nickname == "ADMIN":
-            client.send("PASS".encode("ascii"))
-            password = client.recv(1024).decode("ascii")
-
-            if password != 'mutatismutandis':
-                client.send("REFUSE".encode("ascii"))
-                client.close()
-                continue
-
-        nicknames.append(nickname)
-        clients.append(client)
-
-        client.send("Successfully connected to the server. \n".encode("ascii"))
-        broadcast(f"{nickname} joined the chat. \n".encode("ascii"))
-
-        thread = threading.Thread(target=respond, args=(client,))
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 
-def kick_user(name):
-    if name in nicknames:
-        name_index = nicknames.index(name)
-        client_to_kick = clients[name_index]
-        clients.remove(client_to_kick)
-        client_to_kick.send("You were kicked by ADMIN!".encode("ascii"))
-        nicknames.remove(name)
-        broadcast(f"{name} was kicked by ADMIN!".encode("ascii"))
-
-
-print(f"Server is auth_connection on IP {host}:{port}...\n")
-
-auth_connection()
+print("[STARTING] Server is starting up.")
+start()
